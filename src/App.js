8c9f0c3cfa30391b57edc879ea6840b9bb99212e
Jessweb87 <jprosper87@gmail.com>
2021-09-10 12:03:03 -0400

    completed sprint

--- src/App.js
+++ src/App.js
@@ -2,17 +2,6 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import './App.css';
 import Character from './components/Character';
-// import styled from 'styled-components';
-
-// const Card = styled.div`
-// display: flex;
-// justify-content: space-evenly;
-// margin-top: 30px;
-// align-items: center;
-// padding: 10px;
-// black: black;
-// white: white;
-// `;
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -30,7 +19,7 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-  
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
