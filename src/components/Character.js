4c8ca0fa10eeabf158d28698045bf79023ef34d9
Jessweb87 <jprosper87@gmail.com>
2021-09-10 11:33:31 -0400

    completed tasks

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,17 @@
-// Write your Character component here
+import React, { useState, useEffect } from "react";
+
+class Character extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+    console.log("inside of COM");
+  }
+
+  render() {
+    return <p>{this.props.name}</p>;
+    
+  }
+}
+
+export default Character;
